b6765524a6b1fee2efaa2d044c4199d39e388b8c
anurag-asu <adhasma1@asu.edu>
2021-11-10 19:29:30 -0800

    normalising data, making plots

--- src/results.py
+++ src/results.py
@@ -1,177 +1,171 @@
 from numpy import NaN
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy.stats as stats
+import pylab
+import numpy as np
 
-def pre_process_data_frequency():
-    df = pd.read_csv('csvs/asfi_refined_with_metrics.csv')
-    df = df.loc[(df['frequency'] != NaN) & (df['correct_url_found'] != False)]
-
-    print('initial projects ', len(df))
-
-    # plt.subplot(1,2,1)
-    # plt.title('frequency metric with outliers')
-    # plt.boxplot(df['frequency'])
-    
-    # removing outliers
-    # TO DO Plot boxplot before
-    Q1 = df['frequency'].quantile(0.25)
-    Q3 = df['frequency'].quantile(0.75)
-
-    # print(Q1, Q3)
+def remove_ouliers(df, metric):
+    Q1 = df[metric].quantile(0.25)
+    Q3 = df[metric].quantile(0.75)
 
     IQR = Q3 - Q1    #IQR is interquartile range. 
 
-    filter = (df['frequency'] >= Q1 - 1.5 * IQR) & (df['frequency'] <= Q3 + 1.5 *IQR) 
-    
+    filter = (df[metric] >= Q1 - 1.5 * IQR) & (df[metric] <= Q3 + 1.5 *IQR) 
     df = df.loc[filter]
-    # TO DO Plot boxplot after
 
-    # plt.subplot(1,2,2)
-    # plt.title('frequency metric after removing outliers')
-    # plt.boxplot(df['frequency'])
+    return df
 
-    # plt.show()
 
-    print('projects after removing outliers', len(df))
+def pre_process_data_metric(df, metric):
+    
+    df = normalize_data(df, metric, False)
+    df.replace([np.inf, -np.inf], 0, inplace=True)
+    print(df[metric])
+    mean = df[metric].mean()
 
-    # plot a histogram for visualisation
+    print('mean', mean)
 
-    # plt.xlabel('Frequency of commits')
-    # plt.title('Data distribution for frequency metric')
-    # plt.hist(x=df['frequency'], bins=20, rwidth=0.9)
-    # plt.show()
+    # catrgorize dataframe based on mean and remove status = 0, i.e still in incubation
+    df_mean_high = df.loc[(df[metric] >= mean)]
+    df_mean_low = df.loc[(df[metric] < mean)]
+
+    print('projects with ' + metric + ' higher than mean ', len(df_mean_high))
+    print('projects with ' + metric + ' lower than mean ', len(df_mean_low))
+
+    df_mean_high_gtd = df_mean_high.loc[df['status'] == 1]
+    df_mean_high_rtd = df_mean_high.loc[df['status'] == 2]
+
+    print('projects with ' + metric + ' higher than mean and graduated ', len(df_mean_high_gtd))
+    print('projects with ' + metric + ' higher than mean and retired ', len(df_mean_high_rtd))
+
+    df_mean_low_gtd = df_mean_low.loc[df['status'] == 1]
+    df_mean_low_rtd = df_mean_low.loc[df['status'] == 2]
+
+    print('projects with ' + metric + ' lower than mean and graduated ', len(df_mean_low_gtd))
+    print('projects with ' + metric + ' lower than mean and retired ', len(df_mean_low_rtd))
+
+    chi_sq(len(df_mean_high_gtd),len(df_mean_low_gtd), len(df_mean_high_rtd), len(df_mean_low_rtd))
+
+
+def chi_sq(df_mean_high_gtd, df_mean_low_gtd, df_mean_high_rtd, df_mean_low_rtd):
+    # chi-squared test with similar proportions
+    from scipy.stats import chi2_contingency
+    from scipy.stats import chi2
+
+    # contingency table
+    table = [	
+                [df_mean_high_gtd, df_mean_low_gtd],
+                [df_mean_high_rtd, df_mean_low_rtd]
+            ]
+
+    print(table)
+    stat, p, dof, expected = chi2_contingency(table)
+
+    print('dof=%d' % dof)
+    print(expected)
+    # interpret test-statistic
+
+    prob = 0.95
+    critical = chi2.ppf(prob, dof)
+
+    print('probability=%.3f, critical=%.3f, stat=%.3f' % (prob, critical, stat))
+    if abs(stat) >= critical:
+        print('Dependent (reject H0)')
+    else:
+        print('Independent (fail to reject H0)')
+
+    # interpret p-value
+    alpha = 0.025
+
+    print('significance=%.3f, p=%.3f' % (alpha, p))
+    if p <= alpha:
+        print('Dependent (reject H0)')
+    else:
+        print('Independent (fail to reject H0)')
+
+
+def plot_outlier_and_distribution_graphs(df, metric):
+
+    plt.subplot(2,2,1)
+    plt.title(metric + ' metric with outliers')
+    plt.boxplot(df[metric])
+    
+    df = remove_ouliers(df, metric)
+
+    plt.subplot(2,2,2)
+    plt.title(metric + ' metric after removing outliers')
+    plt.boxplot(df[metric])
 
     # calculate std deviation and mean
-    std = df['frequency'].std()
-    mean = df['frequency'].mean()
+    std = df[metric].std()
+    mean = df[metric].mean()
+
+    plt.subplot(2,2,3)
 
     # Plot pandas histogram from dataframe with df.plot.hist (not df.hist)
-    ax = df['frequency'].plot.hist(bins=20, density=True, edgecolor='w', linewidth=0.5)
+    ax = df[metric].plot.hist(bins=20, density=True, edgecolor='w', linewidth=0.5)
 
     # Save default x-axis limits for final formatting because the pandas kde
     # plot uses much wider limits which usually decreases readability
     xlim = ax.get_xlim()
 
     # Plot pandas KDE
-    df['frequency'].plot.kde(color='k', alpha=0.5, ax=ax) # same as df['var'].plot.kde()
+    df[metric].plot.kde(color='k', alpha=0.5, ax=ax) # same as df['var'].plot.kde()
 
     # Reset x-axis limits and edit legend and add title
     ax.set_xlim(xlim)
     ax.legend(labels=['KDE'], frameon=False)
-    ax.set_title('Data distribution of frequency metric', fontsize=14, pad=15)
-    ax.set_xlabel('Frequency metric')
+    ax.set_title('Data distribution of ' + metric + ' metric', fontsize=14, pad=15)
+    ax.set_xlabel(metric + ' metric')
     ax.text(0.7, 0.8, 'Mean = ' + str(mean) +'\nStd = ' + str(std), horizontalalignment='center',
         verticalalignment='center', transform=ax.transAxes)
+    
+    plt.subplot(2,2,4)
+    stats.probplot(df[metric], dist="norm", plot=pylab)
+
     plt.show()
-
-    print('mean of frequency metric', mean)
-
-    # catrgorize dataframe based on mean and remove status = 0, i.e still in incubation
-    df_mean_high = df.loc[(df['frequency'] >= mean) & (df['status'] != 0)]
-    df_mean_low = df.loc[(df['frequency'] < mean) & (df['status'] != 0)]
-
-    print('projects with frequency higher than mean ', len(df_mean_high))
-    print('projects with frequency lower than mean ', len(df_mean_low))
-
-    df_mean_high_gtd = df_mean_high.loc[df['status'] == 1]
-    df_mean_high_rtd = df_mean_high.loc[df['status'] == 2]
-
-    print('projects with frequency higher than mean and graduated ', len(df_mean_high_gtd))
-    print('projects with requency higher than mean and retired ', len(df_mean_high_rtd))
-
-    df_mean_low_gtd = df_mean_low.loc[df['status'] == 1]
-    df_mean_low_rtd = df_mean_low.loc[df['status'] == 2]
-
-    print('projects with frequency lower than mean and graduated ', len(df_mean_low_gtd))
-    print('projects with frequency lower than mean and retired ', len(df_mean_low_rtd))
+    pylab.show()
 
 
-def pre_process_data_dimensionality():
+def normalize_data(df, metric, show_graph = False):
+    df[metric] = np.log(df[metric])
+    
+    if show_graph:
+        stats.probplot(df[metric], dist="norm", plot=pylab)
+        pylab.show()
+
+    return df
+
+def pre_process_data(metric):
     df = pd.read_csv('csvs/asfi_refined_with_metrics.csv')
-    df = df.loc[(df['dimensionality'] != NaN) & (df['correct_url_found'] != False)]
+    df = df.loc[(df[metric] != NaN) & (df['correct_url_found'] != False) & (df['status'] != 0)]
+    pre_process_data_metric(df, metric)
 
-    print('initial projects ', len(df))
+def plot_graphs(metric):
+    df = pd.read_csv('csvs/asfi_refined_with_metrics.csv')
+    df = df.loc[(df[metric] != NaN) & (df['correct_url_found'] != False) & (df['status'] != 0)]
+    plot_outlier_and_distribution_graphs(df, metric)
 
-    # plt.subplot(1,2,1)
-    # plt.title('dimensionality metric with outliers')
-    # plt.boxplot(df['dimensionality'])
-    
-    # removing outliers
-    # TO DO Plot boxplot before
-    Q1 = df['dimensionality'].quantile(0.25)
-    Q3 = df['dimensionality'].quantile(0.75)
+def normalize_metrics(metric, show_graph = False):
+    df = pd.read_csv('csvs/asfi_refined_with_metrics.csv')
+    df = df.loc[(df[metric] != NaN) & (df['correct_url_found'] != False) & (df['status'] != 0)]
+    df = normalize_data(df, metric, show_graph)
 
-    # print(Q1, Q3)
+# chi_sq()
 
-    IQR = Q3 - Q1    #IQR is interquartile range. 
+pre_process_data('frequency')
+pre_process_data('dimensionality')
+# pre_process_data_frequency()
 
-    filter = (df['dimensionality'] >= Q1 - 1.5 * IQR) & (df['dimensionality'] <= Q3 + 1.5 *IQR) 
-    
-    df = df.loc[filter]
-    # TO DO Plot boxplot after
-    # plt.subplot(1,2,2)
-    # plt.title('dimensionality metric with outliers')
-    # plt.boxplot(df['dimensionality'])
+# plot_graphs('frequency')
 
-    print('projects after removing outliers', len(df))
-
-    # plot a histogram for visualisation
-
-    # plt.xlabel('Dimensionality metric')
-    # plt.title('Data distribution for dimensionality metric')
-    # plt.hist(x=df['dimensionality'], bins=20, rwidth=0.9)
-    # plt.show()
+# normalize_metrics('frequency', True)
+# normalize_metrics('dimensionality', True)
 
     
+ 
 
-    # calculate std deviation and mean
-    std = df['dimensionality'].std()
-    mean = df['dimensionality'].mean()
-
-    # Plot pandas histogram from dataframe with df.plot.hist (not df.hist)
-    ax = df['dimensionality'].plot.hist(bins=20, density=True, edgecolor='w', linewidth=0.5)
-
-    # Save default x-axis limits for final formatting because the pandas kde
-    # plot uses much wider limits which usually decreases readability
-    xlim = ax.get_xlim()
-
-    # Plot pandas KDE
-    df['dimensionality'].plot.kde(color='k', alpha=0.5, ax=ax) # same as df['var'].plot.kde()
-
-    # Reset x-axis limits and edit legend and add title
-    ax.set_xlim(xlim)
-    ax.legend(labels=['KDE'], frameon=False)
-    ax.set_title('Data distribution of dimensionality metric', fontsize=14, pad=15)
-    ax.set_xlabel('Dimentionality metric')
-    ax.text(0.7, 0.8, 'Mean = ' + str(mean) +'\nStd = ' + str(std), horizontalalignment='center',
-        verticalalignment='center', transform=ax.transAxes)
-    plt.show()
-
-
-    print('mean of frequency metric', mean)
-
-    # catrgorize dataframe based on mean and remove status = 0, i.e still in incubation
-    df_mean_high = df.loc[(df['dimensionality'] >= mean) & (df['status'] != 0)]
-    df_mean_low = df.loc[(df['dimensionality'] < mean) & (df['status'] != 0)]
-
-    print('projects with dimensionality higher than mean ', len(df_mean_high))
-    print('projects with dimensionality lower than mean ', len(df_mean_low))
-
-    df_mean_high_gtd = df_mean_high.loc[df['status'] == 1]
-    df_mean_high_rtd = df_mean_high.loc[df['status'] == 2]
-
-    print('projects with dimensionality higher than mean and graduated ', len(df_mean_high_gtd))
-    print('projects with dimensionality higher than mean and retired ', len(df_mean_high_rtd))
-
-    df_mean_low_gtd = df_mean_low.loc[df['status'] == 1]
-    df_mean_low_rtd = df_mean_low.loc[df['status'] == 2]
-
-    print('projects with dimensionality lower than mean and graduated ', len(df_mean_low_gtd))
-    print('projects with dimensionality lower than mean and retired ', len(df_mean_low_rtd))
-
-
-# pre_process_data_dimensionality()
-pre_process_data_frequency()
 
 
 
