cd64e0c879169cf708c98ddcbfb806e3b93cf771
Kiranpreet Kaur <35642755+kpkaur28@users.noreply.github.com>
2021-11-20 22:28:45 -0800

    Fixed bugs - code to collect metrics data

--- metrics.py
+++ metrics.py
@@ -17,7 +17,7 @@ headers = {
 ##optimization
 
 def get_repos():
-	df = pd.read_csv("csv/asfi_refined_300_proj.csv") 
+	df = pd.read_csv("csv/asfi_refined_false_removed.csv") 
 	project_list = []
 	project_list = df.pj_github_api_url
 	print("No. of projects:", len(project_list))
@@ -25,48 +25,57 @@ def get_repos():
 
 def get_forks(project):
 	page_num = 1
+	path = project+"/forks?page={}&per_page=100".format(page_num)
+
+	resp = requests.get(path, headers=headers)
+	resp.raise_for_status()
+	resp_json = resp.json()
+
 	forks_url = []
-	try:
-		resp = requests.get(project+"/forks?sort=created&direction=asc&page={}&per_page=100&state=all".format(page_num), headers=headers)
+
+	for resp in resp_json:
+		forks_url.append(resp['url'])
+		
+	while(len(resp_json) == 100):
+		page_num = page_num + 1
+		path = project+"/forks?page={}&per_page=100".format(page_num)
+
+		resp= requests.get(path, headers=headers)
+		resp.raise_for_status()
 		resp_json = resp.json()
 
-		if resp.status_code == 404:
-			return 0
-		else:
-			while(len(resp_json) == 100):
-				page_num = page_num + 1
-				response = requests.get(project+"/forks?sort=created&direction=asc&page={}&per_page=100&state=all".format(page_num), headers=headers).json()
-				for resp in response: 
-					forks_url.append(resp['url'])
-				resp_json = response
-			print("Total No. of forks:", len(forks_url))
-			return forks_url
-
-	except Exception as e:
-		print("In get_forks function")
-		print('{}'.format(e))
+		for resp in resp_json: 
+			forks_url.append(resp['url'])
+		
+	#print("Total No. of forks:", len(forks_url))
+	return forks_url
 
 def get_pulls(url):
 	page_num = 1
+	path = url+"/pulls?page={}&per_page=100".format(page_num)
+
+	resp = requests.get(path, headers=headers)
+	resp.raise_for_status()
+	resp_json = resp.json()
+
 	pulls_url = []
-	try:
-		resp = requests.get(url+"/pulls?sort=created&direction=asc&page={}&per_page=100&state=all".format(page_num), headers=headers)
+
+	for resp in resp_json:
+		pulls_url.append(resp['url'])
+		
+	while(len(resp_json) == 100):
+		page_num = page_num + 1
+		path = url+"/pulls?page={}&per_page=100".format(page_num)
+		
+		resp = requests.get(path, headers=headers)
+		resp.raise_for_status()
 		resp_json = resp.json()
-		if resp.status_code == 404:
-			return 0
-		else:
-			while(len(resp_json) == 100):
-				response = requests.get(url+"/pulls?sort=created&direction=asc&page={}&per_page=100&state=all".format(page_num), headers=headers).json()
-				for resp in response:
-					pulls_url.append(resp['url'])
-				print("Pulls are:", len(pulls_url))
-				resp_json = response
-			print("Total No. of pulls:", len(pulls_url))
-			return pulls_url
-			
-	except Exception as e:
-		print("In get_pulls function")
-		print('{}'.format(e))
+
+		for resp in resp_json:
+			pulls_url.append(resp['url'])
+		
+	#print("Total No. of pulls:", len(pulls_url))
+	return pulls_url
 	
 def get_merge_status(pull_url):
 	response = requests.get(pull_url+"/merge", headers=headers)
@@ -76,7 +85,7 @@ def get_merge_status(pull_url):
 
 ## Metric 1 - Presence of hard forks
 def check_for_hard_forks():
-	df = pd.read_csv("csv/asfi_refined_300_proj.csv")
+	df = pd.read_csv("csv/asfi_refined_false_removed.csv")
 	df['has_hard_fork'] = NaN
 	df['total_forks'] = NaN
 
@@ -85,54 +94,66 @@ def check_for_hard_forks():
 		merged_pr = 0
 		try:
 			forks = get_forks(repo)
-			if forks == 0:
-				len(forks) == 0
+			total_forks = len(forks)
+			df.loc[df['pj_github_api_url'] == repo, 'total_forks'] = total_forks
+			
+			if total_forks == 0:
 				is_hard_fork = False
 				df.loc[df['pj_github_api_url'] == repo, 'has_hard_fork'] = is_hard_fork
 				continue
-			df.loc[df['pj_github_api_url'] == repo, 'total_forks'] = len(forks)
-			'''
+			
 			for fork in forks:
-				pulls_data = get_pulls(fork)
-				if pulls_data == [] or pulls_data == 0:
-					is_hard_fork = False
-					df.loc[df['pj_github_api_url'] == repo, 'has_hard_fork'] = is_hard_fork
-					continue
-				for pull_request in pulls_data:
-					merge_status = get_merge_status(pull_request)
-					## PR is successfully merged
-					if merge_status == 204:
-						merged_pr += 1
-				if merged_pr >= 2:
-					is_hard_fork = True
-					df.loc[df['pj_github_api_url'] == repo, 'has_hard_fork'] = is_hard_fork
-					print(repo, fork, is_hard_fork)
-			'''
+				try:
+					pulls_data = get_pulls(fork)
+					total_prs = len(pulls_data)
+					if (total_prs == 0):
+						is_hard_fork = False
+						df.loc[df['pj_github_api_url'] == repo, 'has_hard_fork'] = is_hard_fork
+						continue
+					for pull_request in pulls_data:
+						merge_status = get_merge_status(pull_request)
+						## PR is successfully merged
+						if merge_status == 204:
+							print("Merged PR")
+							merged_pr += 1
+					print(merged_pr)
+					if merged_pr >= 2:
+						is_hard_fork = True
+						df.loc[df['pj_github_api_url'] == repo, 'has_hard_fork'] = is_hard_fork
+				
+				except Exception as e: 
+					print("Fork Failed:", fork)
+					print('{}'.format(e))
+					pass
+
 		except Exception as e: 
 			print("Repo Failed:", repo)
 			print('{}'.format(e))
 			pass
 
-		df.to_csv("csv/asfi_refined_300_proj.csv", index=False)
+		print(repo, is_hard_fork)
+		df.to_csv("csv/asfi_refined_false_removed.csv", index=False)
 
 def get_comments(pull_url):
 	page_num = 1
-	response = requests.get(pull_url+"/comments?sort=created&direction=asc&page={}&per_page=100&state=all".format(page_num), headers=headers).json()
+	response = requests.get(pull_url+"/comments?page={}&per_page=100".format(page_num), headers=headers).json()
 	resp_json = response
 	comments_list = []
 	
+	for resp in resp_json: 
+		comments_list.append(resp["body"])
+
 	while(len(resp_json) == 100):
 		page_num = page_num + 1
-		response = requests.get(pull_url+"/comments?sort=created&direction=asc&page={}&per_page=100&state=all".format(page_num), headers=headers).json()
+		response = requests.get(pull_url+"/comments?page={}&per_page=100".format(page_num), headers=headers).json()
 		for resp in response: 
 			comments_list.append(resp["body"])
-		#print(comments_list)
 		resp_json = response
 	return comments_list
 
 def pattern_matching(comment):
 	
-	pattern = r"(clos(e|ed|ing)|dup(licate(d)?|e)?|super(c|s)ee?ded?|obsoleted?|replaced|redundant|better(implementation|solution)|already solved|solved already|addressed already)"
+	pattern = r"(clos(e|ed|ing)|dup(licate(d)?|e)?|super(c|s)ee?ded?|obsoleted?|replaced|redundant|better(implementation|solution)|already(solved|addressed)|(solved|addressed)already)|fixed|done|already"
 	result = re.match(pattern, comment)
 	duplicate_pr = 0
 
@@ -145,9 +166,9 @@ def pattern_matching(comment):
 
 ## Metric 2 - Ratio of duplicate PRs
 def ratio_of_duplicate_prs():
-	df = pd.read_csv("csv/asfi_refined_300_proj.csv")
+	df = pd.read_csv("csv/asfi_refined_false_removed.csv")
 	df['ratio_of_duplicate_prs'] = NaN
-	df['total_pull_requests'] = NaN
+	df['total_prs'] = NaN
 	df['duplicate_prs'] = NaN
 
 	repos = get_repos()
@@ -155,17 +176,11 @@ def ratio_of_duplicate_prs():
 		duplicate_prs = 0
 		try:
 			pulls_data = get_pulls(repo)
-			if pulls_data == [] or pulls_data == 0:
-				ratio = 0
-				df.loc[df['pj_github_api_url'] == repo, 'ratio_of_duplicate_prs'] = ratio
-				df.loc[df['pj_github_api_url'] == repo, 'total_pull_requests'] = total_prs
-				df.loc[df['pj_github_api_url'] == repo, 'duplicate_prs'] = duplicate_prs
-				continue
-			total_prs = len(get_pulls(repo))
+			total_prs = len(pulls_data)
 			if total_prs == 0:
 				ratio = 0
 				df.loc[df['pj_github_api_url'] == repo, 'ratio_of_duplicate_prs'] = ratio
-				df.loc[df['pj_github_api_url'] == repo, 'total_pull_requests'] = total_prs
+				df.loc[df['pj_github_api_url'] == repo, 'total_prs'] = total_prs
 				df.loc[df['pj_github_api_url'] == repo, 'duplicate_prs'] = duplicate_prs
 				continue
 
@@ -177,7 +192,7 @@ def ratio_of_duplicate_prs():
 			
 			ratio = duplicate_prs / total_prs
 			df.loc[df['pj_github_api_url'] == repo, 'ratio_of_duplicate_prs'] = ratio
-			df.loc[df['pj_github_api_url'] == repo, 'total_pull_requests'] = total_prs
+			df.loc[df['pj_github_api_url'] == repo, 'total_prs'] = total_prs
 			df.loc[df['pj_github_api_url'] == repo, 'duplicate_prs'] = duplicate_prs
 			print(repo, ratio, "=", duplicate_prs , "/", total_prs)
 
@@ -186,19 +201,19 @@ def ratio_of_duplicate_prs():
 			print('{}'.format(e))
 			pass
 
-		df.to_csv("csv/asfi_refined_300_proj.csv", index=False)
+		df.to_csv("csv/asfi_refined_false_removed.csv", index=False)
 
 def check_rate_limit():
 	resp = requests.get("https://api.github.com/rate_limit", headers=headers).json()
 	#print(resp)
 	print(resp["rate"])
 
-#check_rate_limit()
+check_rate_limit()
 #check_for_hard_forks()
 #ratio_of_duplicate_prs()
 #get_forks("https://api.github.com/repos/apache/incubator-DolphinScheduler")
-get_pulls("https://api.github.com/repos/apache/incubator-Doris")
-get_comments("")
+#get_pulls("https://api.github.com/repos/apache/incubator-retired-amaterasu")
+#get_comments("")
 
 #Testing
 #get_forks("https://api.github.com/repos/apache/incubator-retired-amaterasu")
